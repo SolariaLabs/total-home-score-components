f859d84ea3ca2ba74e951bb4eec7c1686f24cdea
Vijay Francis <Vijay.Francis@libertymutual.com>
2018-06-24 13:14:43 -0400

    Update debug logging logic

--- packages/common/src/analytics/index.ts
+++ packages/common/src/analytics/index.ts
@@ -1,34 +1,26 @@
 import { sendEvent } from '../api';
-import { getUser, isDebugEnabled } from '../utils';
+import { getUser, log, isDebugEnabled } from '../utils';
 import { Event } from '../types';
 
 export const track = (name: string, event: Event = {}, store: boolean = false) => {
   if (store) {
     if (localStorage.getItem(`ths_widget.tracking.${name}`)) {
-      if (isDebugEnabled()) {
-        console.log(`Skipping event [${name}]. Event previously sent.`);
-      }
+      log(`Skipping event [${name}]. Event previously sent.`);
       return Promise.resolve();
     }
     localStorage.setItem(`ths_widget.tracking.${name}`, 'true');
   }
 
-  if (isDebugEnabled()) {
-    console.log(`Event: [${JSON.stringify({ name, ...event, user: getUser() })}]`);
-    return Promise.resolve();
-  }
+  log(`Event: [${JSON.stringify({ name, ...event, user: getUser() })}]`);
   
-  return sendEvent([{ name, ...event, user: getUser() }])
-    .then(response => response.text())
-    .then((text: string) => {
-      if (isDebugEnabled()) {
-        console.log(`Event [${name}] sent. Status: ${text}`);
-      }
-    })
-    .catch((err: any) => {
-      if (isDebugEnabled()) {
-        console.error(`Error sending event: [${name}]`);
-        console.error(err);
-      }
-    });
+  return isDebugEnabled()
+  ? Promise.resolve()
+  : sendEvent([{ name, ...event, user: getUser() }])
+      .then(response => response.text())
+      .then((text: string) => {
+        log(`Event [${name}] sent. Status: ${text}`);
+      })
+      .catch((err: any) => {
+        log(`Error sending event: [${name}]`);
+      });
 };
--- packages/common/src/utils/debug.ts
+++ packages/common/src/utils/debug.ts
@@ -1,2 +1,5 @@
 export const isDebugEnabled = () =>
   localStorage.getItem('ths_widget.debug') || false;
+
+export const log = (message: string) =>
+  (isDebugEnabled() && console.log(message));
